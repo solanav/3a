1bdfb5670fcac014dcd9a3b1fe805a235958fd92
solanav <solanav@qq.com>
2020-04-27 17:21:06 +0200

    Init

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+use graphics::clear;
+use graphics::*;
+
+pub struct App {
+    gl: GlGraphics,
+    pos: (f64, f64),
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+
+        let enemy = ellipse::circle(0.0, 0.0, 2.0);
+        let aim = ellipse::circle(0.0, 0.0, 2.0);
+        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(GREEN, gl);
+
+            let transform = c
+                .transform
+                .trans(x, y);
+
+            // Draw the enemy and the aim
+            ellipse(RED, enemy, transform, gl);
+            ellipse(BLUE, enemy, transform, gl);
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+
+    }
+}
+
+fn main() {
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new("aime", [200, 200])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        pos: (0.0, 0.0)
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+
+        if let Some(args) = e.update_args() {
+            app.update(&args);
+        }
+    }
+}
